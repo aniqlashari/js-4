806523837ff6acca246289a9a7c7bdf7982ad9a4
aniqlashari <mahaqamber3@gmail.com>
2024-07-02 21:15:14 +0500

    first commit

---
+++ js.js
@@ -0,0 +1,113 @@
+// var email = prompt ("enter your email" )
+// var password =  prompt ("password")
+
+
+
+// if ( email === "kashan@gmail.com" && password === "admin"){ 
+// alert("you are kashan")
+// }
+// else(
+
+// alert("you are user")
+
+
+
+// )
+alert ("you have no wins lets start game to win and get more wins")
+var quiestion1 =+ prompt ("4 + 4 = ?")
+var answer = 8
+
+
+
+if (quiestion1 === answer){
+
+
+alert ("you get 1 win")
+
+
+
+}
+
+else{
+
+
+alert("you lose 1")
+
+
+
+}
+
+var quiestion2 =+ prompt ("12 X 1 = ? ")
+var answer1 = 12
+if (quiestion2===answer1){
+
+alert ("you win 2")
+
+}
+else{
+
+
+    alert("you lose 2")
+}
+
+var quiestion3 =+ prompt ("9 + 9 = ?")
+var answer3 = 18
+
+if (quiestion3 === answer3){ alert("you get 3 wins")}
+else{
+
+    alert("you lose 3")
+}
+
+var quiestion4 =+ prompt ("11 X 5 = ? ")
+var answer4 = 55
+
+
+
+
+
+
+
+
+
+
+if (quiestion4 ===answer4){ alert("you get 4 wins")}
+else{
+
+    alert("you lose 4")
+}
+
+var quiestion5 =+ prompt ("10 X 5 = ? ")
+var answer5 = 50
+
+
+
+
+
+
+
+
+
+
+if (quiestion5 ===answer5){ alert("you get 5 wins")}
+else{
+
+    alert("you lose 5")
+}
+var quiestion6 =+ prompt ("11 + 5 = ? ")
+var answer6 = 16
+
+
+
+
+
+
+
+
+
+
+if (quiestion6 ===answer6){ alert("you get 6 wins last you are op😈😈😈😈😈")}
+else{
+
+    alert("you lose 6")
+}
